553071ec8689da268d6241ff6d9500803a9ab946
Duru <dk2565@columbia.edu>
2017-01-26 15:44:27 -0500

    Added my homework

---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import sys
+
+arg1 = int(sys.argv[1])
+arg2 = int(sys.argv[2])
+
+result = arg1 * arg2
+print result
